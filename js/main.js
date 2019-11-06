e170fe0cc275cc8840c53521e487a47b237ad119
Alex Chalyi <lexa2905@gmail.com>
2019-11-06 20:55:12 +0300

    owl-carousel +ipad

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
 $('.menu-btn').on('click', function () {
     $('.nav').toggleClass('is-active');
 });
+
+$(document).ready(function(){
+
+    $('.owl-carousel').owlCarousel({
+
+        responsive:{
+            0:{
+                items:2
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    });
+});
